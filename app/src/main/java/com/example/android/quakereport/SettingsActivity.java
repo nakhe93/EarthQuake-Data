b93c1f64771d4ccdf6aa849166b037ca0474e7ae
AdhirajNakhe <nakhe93@ufl.edu>
2017-08-28 16:32:41 -0400

    Added the back button to Search settings activity

--- app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -1,10 +1,12 @@
 package com.example.android.quakereport;
 
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
 
@@ -17,6 +19,9 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
     public static class EarthquakePreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener{
@@ -29,6 +34,8 @@ public class SettingsActivity extends AppCompatActivity {
             Preference maxMagnitude = findPreference(getString(R.string.settings_max_magnitude_key));
             bindPreferenceSummaryToValue(minMagnitude);
             bindPreferenceSummaryToValue(maxMagnitude);
+
+
         }
 
         public boolean onPreferenceChange(Preference preference,Object value){
